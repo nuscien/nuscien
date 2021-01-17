ba46586a595dcb8b967e553e6e7ef45b7e6f52f2
Kingcean Tuan <kingcean@live.com>
2021-01-17 14:05:05 +0800

    Update the SSMS project.

--- Sql/Initialization/TestTabelsDeletion.sql
+++ Sql/Initialization/TestTabelsDeletion.sql
@@ -1,6 +1,10 @@
 USE [NuScien5]
 GO
 
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[testcustomer]') AND type in (N'U'))
-DROP TABLE [dbo].[testcustomer]
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[testcustomers]') AND type in (N'U'))
+DROP TABLE [dbo].[testcustomers]
+GO
+
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[testgoods]') AND type in (N'U'))
+DROP TABLE [dbo].testgoods
 GO
